354e593118dad8b0893b068fa55a755fef96d58b
Thanveer KP <thanveerkp99@gmail.com>
2024-11-22 11:31:55 +0530

    Character printing

---
+++ lib/Assignments/character.dart
@@ -0,0 +1,14 @@
+
+import 'dart:io';
+
+// 1.	Accept a char input from the user and display it on the console.
+
+void main() {
+
+print('Enter a Character : ');
+String char = stdin.readLineSync()!;
+print('Entered character is $char');
+
+
+
+}
