c5209ac70cc67fb5f80b9286e788dc0da6698388
Thanveer KP <thanveerkp99@gmail.com>
2024-11-22 11:19:42 +0530

    added new folder Assignment

---
+++ lib/Assignments/prime_number.dart
@@ -0,0 +1,21 @@
+
+import 'dart:io';
+
+void main() {
+//! check Prime number
+
+  print('enter your number');
+  num number = int.parse(stdin.readLineSync()!);
+
+  int i;
+  for (i = 2; i < number; i++) {
+    if (number % i == 0) {
+      break;
+    }
+  }
+  if (i == number) {
+    print('prime number');
+  } else {
+    print('not prime');
+  }
+}
