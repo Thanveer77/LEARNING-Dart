0c8ccc6fea55adf981fcdca4bff2207aeca005bf
Thanveer KP <thanveerkp99@gmail.com>
2024-12-02 12:03:48 +0530

    Sorting in Ascending Order

---
+++ lib/Assignments/sort_ascending.dart
@@ -0,0 +1,17 @@
+// Sorting in Ascending Order
+
+void main() {
+  List<int> array = [3, 9, 6, 2, 7, 1, 5, 7];
+
+  for (int i = 0; i < array.length; i++) {
+    for (int j = i + 1; j < array.length; j++) {
+      if (array[i] > array[j]) {
+        // Swap elements
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+      }
+    }
+  }
+  print(array);
+}
